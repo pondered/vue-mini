8fbcefec7606e672892604685cd3de6219a5e990
Ponder <example>
2023-12-01 06:00:58 +0800

    finish render diff

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -138,6 +138,7 @@ function baseCreateRenderer(options: RenderOptions): any {
     if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
       hostSetElementText(el, vnode.children)
     } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+      mountChildren(vnode.children, el, anchor)
     }
 
     if (props) {
@@ -170,22 +171,23 @@ function baseCreateRenderer(options: RenderOptions): any {
   }
 
   const patchChildren = (oldVNode, newVNode, container, anchor) => {
-    const c1 = oldVNode && oldVNode.children
+    const oldChildren = oldVNode && oldVNode.children
     const prevShapeFlag = oldVNode ? oldVNode.shapeFlag : 0
-    const c2 = newVNode && newVNode.children
+    const newChildren = newVNode && newVNode.children
     const { shapeFlag } = newVNode
 
     if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
       if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
         // 卸载旧子节点
       }
-      if (c2 !== c1) {
-        hostSetElementText(container, c2)
+      if (newChildren !== oldChildren) {
+        hostSetElementText(container, newChildren)
       }
     } else {
       if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
         if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
           // diff
+          patchKeyedChildren(oldChildren, newChildren, container, anchor)
         } else {
         }
       } else {
@@ -197,6 +199,163 @@ function baseCreateRenderer(options: RenderOptions): any {
         }
       }
     }
+  }
+  const patchKeyedChildren = (
+    oldChildren,
+    newChildren,
+    container,
+    parentAnchor
+  ) => {
+    let i = 0
+    const newChildrenLenght = newChildren.length
+    let oldChildrenEnd = oldChildren.length - 1
+    let newChildrenEnd = newChildrenLenght - 1
+
+    // 自前向后
+    while (i <= oldChildrenEnd && i <= newChildrenEnd) {
+      const oldVNode = oldChildren[i]
+      const newVNode = normalizeVNode(newChildren[i])
+      if (isSameVNodeType(oldVNode, newVNode)) {
+        patch(oldVNode, newVNode, container, null)
+      } else {
+        break
+      }
+      i++
+    }
+
+    // 自后向前
+    while (i <= oldChildrenEnd && i <= newChildrenEnd) {
+      const oldVNode = oldChildren[oldChildrenEnd]
+      const newVNode = newChildren[newChildrenEnd]
+
+      if (isSameVNodeType(oldVNode, newVNode)) {
+        patch(oldVNode, newVNode, container, null)
+      } else {
+        break
+      }
+      oldChildrenEnd--
+      newChildrenEnd--
+    }
+
+    // 新节点多余就节点
+    if (i > oldChildrenEnd) {
+      if (i <= newChildrenEnd) {
+        const nextPos = newChildrenEnd + 1
+        const anchor =
+          nextPos < newChildrenLenght ? newChildren[nextPos].el : parentAnchor
+        while (i <= newChildrenEnd) {
+          patch(null, normalizeVNode(newChildren[i]), container, anchor)
+          i++
+        }
+      }
+    } else if (i > newChildrenEnd) {
+      // 旧节点多余新节点
+      while (i <= oldChildrenEnd) {
+        unmount(oldChildren[i])
+        i++
+      }
+    } else {
+      // 乱序
+      // 5. unknown sequence
+      // [i ... oldChildrenEnd + 1]: a b [c d e] f g
+      // [i ... newChildrenEnd + 1]: a b [e d c h] f g
+      // i = 2, oldChildrenEnd = 4, newChildrenEnd = 5
+      const oldStartIndex = i // prev starting index
+      const newStartIndex = i // next starting index
+
+      // 5.1 build key:index map for newChildren
+      const keyToNewIndexMap: Map<string | number | symbol, number> = new Map()
+      for (i = newStartIndex; i <= newChildrenEnd; i++) {
+        const nextChild = normalizeVNode(newChildren[i])
+        keyToNewIndexMap.set(nextChild.key, i)
+      }
+
+      // 5.2 loop through old children left to be patched and try to patch
+      // matching nodes & remove nodes that are no longer present
+      let j
+      let patched = 0
+      const toBePatched = newChildrenEnd - newStartIndex + 1
+      let moved = false
+      // used to track whether any node has moved
+      let maxNewIndexSoFar = 0
+      // works as Map<newIndex, oldIndex>
+      // Note that oldIndex is offset by +1
+      // and oldIndex = 0 is a special value indicating the new node has
+      // no corresponding old node.
+      // used for determining longest stable subsequence
+      const newIndexToOldIndexMap = new Array(toBePatched)
+      for (i = 0; i < toBePatched; i++) newIndexToOldIndexMap[i] = 0
+
+      for (i = oldStartIndex; i <= oldChildrenEnd; i++) {
+        const prevChild = oldChildren[i]
+        if (patched >= toBePatched) {
+          // all new children have been patched so this can only be a removal
+          unmount(prevChild)
+          continue
+        }
+        let newIndex
+        if (prevChild.key != null) {
+          newIndex = keyToNewIndexMap.get(prevChild.key)
+        } else {
+          // key-less node, try to locate a key-less node of the same type
+          for (j = newStartIndex; j <= newChildrenEnd; j++) {
+            if (
+              newIndexToOldIndexMap[j - newStartIndex] === 0 &&
+              isSameVNodeType(prevChild, newChildren[j])
+            ) {
+              newIndex = j
+              break
+            }
+          }
+        }
+        if (newIndex === undefined) {
+          unmount(prevChild)
+        } else {
+          newIndexToOldIndexMap[newIndex - newStartIndex] = i + 1
+          if (newIndex >= maxNewIndexSoFar) {
+            maxNewIndexSoFar = newIndex
+          } else {
+            moved = true
+          }
+          patch(prevChild, newChildren[newIndex], container, null)
+          patched++
+        }
+      }
+
+      // 5.3 move and mount
+      // generate longest stable subsequence only when nodes have moved
+      const increasingNewIndexSequence = moved
+        ? getSequence(newIndexToOldIndexMap)
+        : []
+      j = increasingNewIndexSequence.length - 1
+      // looping backwards so that we can use last patched node as anchor
+      for (i = toBePatched - 1; i >= 0; i--) {
+        const nextIndex = newStartIndex + i
+        const nextChild = newChildren[nextIndex]
+        const anchor =
+          nextIndex + 1 < newChildrenLenght
+            ? newChildren[nextIndex + 1].el
+            : parentAnchor
+        if (newIndexToOldIndexMap[i] === 0) {
+          // mount new
+          patch(null, nextChild, container, anchor)
+        } else if (moved) {
+          // move if:
+          // There is no stable subsequence (e.g. a reverse)
+          // OR current node is not among the stable sequence
+          if (j < 0 || i !== increasingNewIndexSequence[j]) {
+            move(nextChild, container, anchor)
+          } else {
+            j--
+          }
+        }
+      }
+    }
+  }
+
+  const move = (vnode, container, anchor) => {
+    const { el } = vnode
+    hostInsert(el!, container, anchor)
   }
 
   const patchProps = (el: Element, vnode, oldProps, newProps) => {
@@ -268,4 +427,47 @@ function baseCreateRenderer(options: RenderOptions): any {
   return {
     render,
   }
+}
+
+
+// https://en.wikipedia.org/wiki/Longest_increasing_subsequence
+function getSequence(arr: number[]): number[] {
+  const p = arr.slice()
+  const result = [0]
+  let i, j, u, v, c
+  const len = arr.length
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i]
+    if (arrI !== 0) {
+      j = result[result.length - 1]
+      if (arr[j] < arrI) {
+        p[i] = j
+        result.push(i)
+        continue
+      }
+      u = 0
+      v = result.length - 1
+      while (u < v) {
+        c = (u + v) >> 1
+        if (arr[result[c]] < arrI) {
+          u = c + 1
+        } else {
+          v = c
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1]
+        }
+        result[u] = i
+      }
+    }
+  }
+  u = result.length
+  v = result[u - 1]
+  while (u-- > 0) {
+    result[u] = v
+    v = p[v]
+  }
+  return result
 }
